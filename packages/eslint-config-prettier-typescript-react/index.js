ed2f6a525297f817fb68fe11834de09270898d4d
Tae-gyu Yun <tgyyun@gmail.com>
2022-01-25 09:39:30 +1100

    feat: updated eslint packages

---
+++ packages/eslint-config-prettier-typescript-react/index.js
@@ -0,0 +1,15 @@
+module.exports = {
+    extends: [
+      "airbnb/hooks",
+    ],
+    rules: {
+      // React
+      "react/jsx-filename-extension": ["error", { extensions: [".tsx"] }],
+      "react/function-component-definition": [
+        2,
+        {
+          namedComponents: ["function-declaration", "arrow-function", "function-expression"]
+        },
+      ],
+    },
+  };
